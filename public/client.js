cde5cf02603fd0153ae352648e5c54a132d26a7a
Rica28w8 <ricardinho.brehmer@gmail.com>
2026-08-28 22:13:18 -0300

    Unmute video in client.js

--- public/client.js
+++ public/client.js
@@ -753,7 +753,7 @@ function setupVolumeControl(tile, audioTrack) {
     gainNode.gain.value = 1;
     source.connect(gainNode).connect(ctx.destination);
     tile.gainNode = gainNode;
-    tile.video.muted = true;
+    tile.video.muted = false;
 
     if (tile.volumeSlider) {
       tile.volumeSlider.addEventListener("input", () => {
